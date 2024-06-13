82301a8a1008e1464e318bd178d6139255671723
5ujay <sujaypagam777@gmail.com>
2024-06-13 13:53:40 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+
+const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("server is ready");
+// });
+
+// get list of 5 jokes
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "a joke",
+      content: "first joke",
+    },
+    {
+      id: 2,
+      title: "a joke",
+      content: "second joke",
+    },
+    {
+      id: 3,
+      title: "a joke",
+      content: "third joke",
+    },
+    {
+      id: 4,
+      title: "a joke",
+      content: "fourth joke",
+    },
+    {
+      id: 5,
+      title: "a joke",
+      content: "fifth joke",
+    },
+  ];
+
+  res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`server at http://localhost:${port}`);
+});
